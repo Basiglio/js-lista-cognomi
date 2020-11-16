8713c7aec7d91a8c81f37f145e6423f56cf61747
Emilo <emilio.furnari87@gmail.com>
2020-11-16 14:25:41 +0100

    second push

---
+++ js/myscript.js
@@ -0,0 +1,22 @@
+// Lista Cognomi
+// Chiedere all’utente il cognome,
+// inserirlo in un array con altri cognomi (Es: 'Pesce', ‘Castenetto’, 'Passaro', 'Grimaldi', ‘Rossi’, ‘Verdi’, ‘Bianchi’).
+// Stampa la lista ordinata alfabeticamente.
+// Scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova.
+
+
+// creo array iniziale
+var databaseSurname = ["topolino", "pippo", "pluto", "minnie", "paperino", "clarabella", "paperina"];
+
+// chiedo all'utente il cognome
+var cognome = prompt("qual'è il tuo cognome?");
+console.log("questo è il cognome: " + cognome);
+
+// stampo i cognomi dell'array
+for (var i = 0; i < databaseSurname.length; i++) {
+  console.log(databaseSurname[i]);
+}
+
+// inserisco nell' arrey il mio cognome
+databaseSurname.push(cognome)
+console.log(databaseSurname);
